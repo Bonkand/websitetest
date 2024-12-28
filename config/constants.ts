1cf83a63bd58212b84b5258faa141264c150c9c6
Bonkand <77266884+Bonkand@users.noreply.github.com>
2024-12-28 17:50:32 +0100

    Add files via upload

---
+++ config/constants.ts
@@ -0,0 +1,13 @@
+export default {
+  social: {
+    discord: "https://discord.gg/rpvNmR9cP9",
+    twitter: "https://twitter.com/eggsydev/",
+    github: "https://github.com/eggsy/",
+    linkedIn: "https://linkedin.com/in/abdulbaki-dursun",
+    trello: "https://trello.com/b/XtWZFFiH/",
+    email: "eggsydev@gmail.com",
+  },
+  sponsor: {
+    github: "https://github.com/sponsors/eggsy",
+  },
+}
